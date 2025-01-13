119628773eacf448295b54c9989680a6040d8f8e
landeo29 <joslan2906@gmail.com>
2025-01-13 17:29:14 -0500

    subiendo nuevos cambios tava inspirado cmr

---
+++ assets/js/colaboracion.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('.collaboration-card').forEach((card) => {
+    card.addEventListener('mousemove', (e) => {
+        const rect = card.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        card.style.setProperty('--mouse-x', `${x}px`);
+        card.style.setProperty('--mouse-y', `${y}px`);
+    });
+
+    card.addEventListener('mouseleave', () => {
+        card.style.setProperty('--mouse-x', `50%`);
+        card.style.setProperty('--mouse-y', `50%`);
+    });
+});
